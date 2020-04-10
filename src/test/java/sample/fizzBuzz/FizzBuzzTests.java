90ba195a8162362b0fce9ef1ff83aaac975c7e31
yuya.sasaki03 <yuya.sasaki03@g.softbank.co.jp>
2020-04-10 13:32:28 +0900

    [add] test case

--- src/test/java/sample/fizzBuzz/FizzBuzzTests.java
+++ src/test/java/sample/fizzBuzz/FizzBuzzTests.java
@@ -11,4 +11,25 @@ class FizzBuzzTests {
         String actual = target.execute(2);
         assertThat(actual).isEqualTo("2");
     }
+
+    @Test
+    void case3() {
+        FizzBuzz target = new FizzBuzz();
+        String actual = target.execute(3);
+        assertThat(actual).isEqualTo("Fizz");
+    }
+
+    @Test
+    void case5() {
+        FizzBuzz target = new FizzBuzz();
+        String actual = target.execute(5);
+        assertThat(actual).isEqualTo("Buzz");
+    }
+
+    @Test
+    void case15() {
+        FizzBuzz target = new FizzBuzz();
+        String actual = target.execute(15);
+        assertThat(actual).isEqualTo("FizzBuzz");
+    }
 }
