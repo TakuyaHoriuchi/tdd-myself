c2242815988601e12a51792d3d7d14dac3e7f261
itouoti12 <white.sleipnir.jazz@gmail.com>
2020-05-21 02:39:01 +0000

    きれいにした

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## cloud9
 ### test
 `./gradlew test --tests "*FizzBuzzTest*"`
+
+### vendingMachine
+`http://devtesting.jp/tddbc/?TDDBC%E5%A4%A7%E9%98%AA3.0%2F%E8%AA%B2%E9%A1%8C`
--- src/test/java/sample/vendingMachine/VendingMachineTest.java
+++ src/test/java/sample/vendingMachine/VendingMachineTest.java
@@ -4,6 +4,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.TestInstance;
+
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -12,62 +15,69 @@ import java.util.stream.Stream;
 
 
 
+//お金の投入と払い戻し
 class VendingMachineTest {
 
-    //投入金額の総計を取得できる。
-    @Test
-    void test() {
-        VendingMachine vm = new VendingMachine();
-        assertThat(vm.getTotal()).isEqualTo(0);
-    }
-    
-    @Test
-    void insertMoney(){
-        VendingMachine vm = new VendingMachine();
-
-        vm.insertCoin(YEN.TEN);
-        assertThat(vm.getTotal()).isEqualTo(10);
-
-        vm.insertCoin(YEN.FIFTY);
-        assertThat(vm.getTotal()).isEqualTo(60);
-
-        vm.insertCoin(YEN.HUNDRED);
-        assertThat(vm.getTotal()).isEqualTo(160);
-
-        vm.insertCoin(YEN.FIVE_HUNDRED);
-        assertThat(vm.getTotal()).isEqualTo(660);
-
-        vm.insertCoin(YEN.THOUSAND);
-        assertThat(vm.getTotal()).isEqualTo(1660);
-    }
-
-    @Test
-    void insert110YenAndRetrunMoney(){
-        VendingMachine vm = new VendingMachine();
-        vm.insertCoin(YEN.TEN);
-        vm.insertCoin(YEN.HUNDRED);
-        assertThat(vm.getTotal()).isEqualTo(110);
+    @Nested
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    class Step0Andstep1Test{
         
-        assertThat(vm.returnMoney()).isEqualTo(110);
-        assertThat(vm.getTotal()).isEqualTo(0);
-    }
+        @Test
+        void 投入金額の総計を取得できる() {
+            VendingMachine vm = new VendingMachine();
+            assertThat(vm.getTotal()).isEqualTo(0);
+        }
     
-    @ParameterizedTest
-    @MethodSource("insertCoinArgs")
-    void insertCoin(YEN yen, YEN expect) {
-        VendingMachine vm = new VendingMachine();
-        YEN actual = vm.insertCoin(yen);
-        assertThat(actual).isEqualTo(expect);
-        assertThat(vm.getTotal()).isEqualTo(0);
-    }
+        @Test
+        void お金の投入が複数回できる(){
+            VendingMachine vm = new VendingMachine();
     
-    private static Stream<Arguments> insertCoinArgs() {
-        return Stream.of(
-            Arguments.arguments(YEN.ONE, YEN.ONE),
-            Arguments.arguments(YEN.FIVE, YEN.FIVE),
-            Arguments.arguments(YEN.FIVE_THOUSAND, YEN.FIVE_THOUSAND)
-        );
+            vm.insertCoin(YEN.TEN);
+            assertThat(vm.getTotal()).isEqualTo(10);
+    
+            vm.insertCoin(YEN.FIFTY);
+            assertThat(vm.getTotal()).isEqualTo(60);
+
+            vm.insertCoin(YEN.HUNDRED);
+            assertThat(vm.getTotal()).isEqualTo(160);
+
+            vm.insertCoin(YEN.FIVE_HUNDRED);
+            assertThat(vm.getTotal()).isEqualTo(660);
+
+            vm.insertCoin(YEN.THOUSAND);
+            assertThat(vm.getTotal()).isEqualTo(1660);
+        }
+
+        @Test
+        void お金を投入して払い戻し操作を行う(){
+            VendingMachine vm = new VendingMachine();
+            vm.insertCoin(YEN.TEN);
+            vm.insertCoin(YEN.HUNDRED);
+            assertThat(vm.getTotal()).isEqualTo(110);
+            
+            assertThat(vm.returnMoney()).isEqualTo(110);
+            assertThat(vm.getTotal()).isEqualTo(0);
+        }
+    
+        @ParameterizedTest
+        @MethodSource("insertCoinArgs")
+        void 扱えないお金を投入してそのまま釣り銭として出力する(YEN yen, YEN expect) {
+            VendingMachine vm = new VendingMachine();
+            YEN actual = vm.insertCoin(yen);
+            assertThat(actual).isEqualTo(expect);
+            assertThat(vm.getTotal()).isEqualTo(0);
+        }
+    
+        private Stream<Arguments> insertCoinArgs() {
+            return Stream.of(
+                Arguments.arguments(YEN.ONE, YEN.ONE),
+                Arguments.arguments(YEN.FIVE, YEN.FIVE),
+                Arguments.arguments(YEN.FIVE_THOUSAND, YEN.FIVE_THOUSAND)
+            );
+        }
+        
     }
+
 
     @Disabled
     @Test
